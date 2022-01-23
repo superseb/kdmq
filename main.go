b404d12b3601462929b1260abd6c2287339d4589
Sebastiaan van Steenis <mail@superseb.nl>
2022-01-23 23:05:10 +0100

    initial

---
+++ main.go
@@ -0,0 +1,403 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/olekukonko/tablewriter"
+	"github.com/rancher/rke/types/kdm"
+	"github.com/superseb/kdmq/util"
+	"github.com/urfave/cli/v2"
+)
+
+var (
+	Version   = "v0.0.0-dev"
+)
+
+func main() {
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:    "listk8s",
+				Aliases: []string{"lk"},
+				Usage:   "list k8s versions for Rancher version",
+				Action: func(c *cli.Context) error {
+					commandUsage := fmt.Sprintf("Usage: %s <rancher_version> <channel>", c.Command.FullName())
+
+					if c.Args().Len() < 2 {
+						return fmt.Errorf("Not enough parameters\n%s", commandUsage)
+					}
+
+					version := c.Args().Get(0)
+					channel := c.Args().Get(1)
+
+					validChannel, err := util.IsValidChannel(channel)
+					if !validChannel {
+						return fmt.Errorf("Not a valid channel: [%s], error [%v]", channel, err)
+					}
+					var data kdm.Data
+
+					if channel == "embedded" {
+						data, err = util.GetKDMDataFromEmbedded(version)
+						if err != nil {
+							return fmt.Errorf("Error while trying to get KDM data from embedded, error [%v]", err)
+						}
+					} else {
+
+						semVersion, err := util.GetSemverFromString(version)
+						if err != nil {
+							return fmt.Errorf("Not a valid semver version: [%s], error [%v]", version, err)
+						}
+
+						data, err = util.GetKDMDataFromURL(channel, fmt.Sprintf("v%d.%d", semVersion.Major, semVersion.Minor))
+						if err != nil {
+							return fmt.Errorf("Not a valid semver version: [%s], error [%v]", version, err)
+						}
+
+						data, err = util.GetKDMDataFromURL(channel, fmt.Sprintf("v%d.%d", semVersion.Major, semVersion.Minor))
+						if err != nil {
+							return fmt.Errorf("Error while trying to get KDM data, error [%v]", err)
+						}
+					}
+
+					k8sVersions, err := util.GetK8sVersionsForVersion(data, version)
+					if err != nil {
+						return fmt.Errorf("Error while trying to get k8s versions, error [%v]", err)
+					}
+
+					fmt.Printf("Kubernetes versions found for version [%s] in channel [%s]:\n\n%s\n", version, channel, strings.Join(k8sVersions, "\n"))
+					return nil
+
+				},
+			},
+			{
+				Name:    "diffk8s",
+				Aliases: []string{"dk"},
+				Usage:   "diff 2 k8s version",
+				Action: func(c *cli.Context) error {
+					commandUsage := fmt.Sprintf("Usage: %s <rancher_version1> <rancher_version2> <channel1> [channel2]", c.Command.FullName())
+
+					if c.Args().Len() < 3 {
+						return fmt.Errorf("Not enough parameters\n:%s", commandUsage)
+					}
+					version1 := c.Args().Get(0)
+					version2 := c.Args().Get(1)
+					channel1 := c.Args().Get(2)
+					var channel2 string
+					if c.Args().Len() == 4 {
+						channel2 = c.Args().Get(3)
+					}
+
+					validChannel, err := util.IsValidChannel(channel1)
+					if !validChannel {
+						return fmt.Errorf("Not a valid channel1: [%s], error [%v]", channel1, err)
+					}
+					var dataVersion1 kdm.Data
+					var dataVersion2 kdm.Data
+					var customChannel2 bool
+
+					if channel1 == "embedded" {
+						dataVersion1, err = util.GetKDMDataFromEmbedded(version1)
+						if err != nil {
+							return fmt.Errorf("Error while retrieving KDM data for channel [%s], error [%v]", channel1, err)
+						}
+					} else {
+						semVersion1, err := util.GetSemverFromString(version1)
+						if err != nil {
+							return fmt.Errorf("Not a valid semver version: [%s], error [%v]", version1, err)
+						}
+
+						dataVersion1, err = util.GetKDMDataFromURL(channel1, fmt.Sprintf("v%d.%d", semVersion1.Major, semVersion1.Minor))
+						if err != nil {
+							return fmt.Errorf("Error while retrieving KDM data from URL for channel [%s], error [%v]", channel1, err)
+						}
+					}
+					if channel2 != "" {
+						customChannel2 = true
+						validChannel, err := util.IsValidChannel(channel2)
+						if !validChannel {
+							return fmt.Errorf("Not a valid channel2: [%s], error [%v]", channel2, err)
+						}
+
+						if channel2 == "embedded" {
+							dataVersion2, err = util.GetKDMDataFromEmbedded(version2)
+							if err != nil {
+								return fmt.Errorf("Error while retrieving KDM data for channel [%s], error [%v]", channel2, err)
+							}
+						} else {
+							semVersion2, err := util.GetSemverFromString(version2)
+							if err != nil {
+								return fmt.Errorf("Not a valid semver version: [%s], error [%v]", version2, err)
+							}
+
+							dataVersion2, err = util.GetKDMDataFromURL(channel2, fmt.Sprintf("v%d.%d", semVersion2.Major, semVersion2.Minor))
+							if err != nil {
+								return fmt.Errorf("Error while retrieving KDM data from URL for channel [%s], error [%v]", channel2, err)
+							}
+						}
+					}
+					var k8sVersionsVersion2 []string
+
+					k8sVersionsVersion1, err := util.GetK8sVersionsForVersion(dataVersion1, version1)
+					if err != nil {
+						return fmt.Errorf("Error while trying to get k8s versions for [%s], error: [%v]", version1, err)
+					}
+
+					if customChannel2 {
+						k8sVersionsVersion2, err = util.GetK8sVersionsForVersion(dataVersion2, version2)
+						if err != nil {
+							return fmt.Errorf("Error while trying to get k8s versions for [%s], error: [%v]", version1, err)
+						}
+					} else {
+						k8sVersionsVersion2, err = util.GetK8sVersionsForVersion(dataVersion1, version2)
+						if err != nil {
+							return fmt.Errorf("Error while trying to get k8s versions for [%s], error: [%v]", version1, err)
+						}
+
+					}
+					diffK8sVersions := util.Difference(k8sVersionsVersion1, k8sVersionsVersion2)
+					sort.Strings(diffK8sVersions)
+
+					replyMessage := fmt.Sprintf("Kubernetes versions found for version [%s] in channel [%s]:\n\n%s\n", version1, channel1, strings.Join(k8sVersionsVersion1, "\n"))
+					if customChannel2 {
+						replyMessage = fmt.Sprintf("%s\nKubernetes versions found for version [%s] in channel [%s]:\n\n%s\n", replyMessage, version2, channel2, strings.Join(k8sVersionsVersion2, "\n"))
+					} else {
+						replyMessage = fmt.Sprintf("%s\nKubernetes versions found for version [%s] in channel [%s]:\n\n%s\n", replyMessage, version2, channel1, strings.Join(k8sVersionsVersion2, "\n"))
+					}
+					replyMessage = fmt.Sprintf(":%s\nDifference:\n%s\n\n", replyMessage, strings.Join(diffK8sVersions, "\n"))
+					fmt.Printf(replyMessage)
+
+					return nil
+
+				},
+			},
+			{
+				Name:    "listk8simages",
+				Aliases: []string{"lki"},
+				Usage:   "list k8s images",
+				Action: func(c *cli.Context) error {
+					commandUsage := fmt.Sprintf("Usage: %s <k8s_version> <channel_version> <channel>", c.Command.FullName())
+					if c.Args().Len() < 3 {
+						return fmt.Errorf("Not enough parameters\n%s", commandUsage)
+					}
+					k8sVersion := c.Args().Get(0)
+					channelVersion := c.Args().Get(1)
+					channel := c.Args().Get(2)
+
+					validChannel, err := util.IsValidChannel(channel)
+					if !validChannel {
+						return fmt.Errorf("Not a valid channel: [%s], error [%v]", channel, err)
+					}
+
+					validChannelVersion, err := util.IsValidChannelVersion(channelVersion)
+					if !validChannelVersion {
+						return fmt.Errorf("Not a valid channel version: [%s], error [%v]", channelVersion, err)
+					}
+
+					data, err := util.GetKDMDataFromURL(channel, channelVersion)
+					if err != nil {
+						return fmt.Errorf("Error while retrieving KDM data, error [%v]", err)
+					}
+
+					uniqueImages := util.GetUniqueSystemImageList(data.K8sVersionRKESystemImages[k8sVersion])
+
+					fmt.Printf("Images for Kubernetes version [%s] for channel [%s]:\n\n%s\n", k8sVersion, channel, strings.Join(uniqueImages, "\n"))
+					return nil
+
+				},
+			},
+			{
+				Name:    "diffk8simages",
+				Aliases: []string{"dki"},
+				Usage:   "diff 2 k8s version images",
+				Action: func(c *cli.Context) error {
+					commandUsage := fmt.Sprintf("Usage: %s <k8s_version1> <k8s_version2> <channel_version> <channel>", c.Command.FullName())
+					if c.Args().Len() < 4 {
+						return fmt.Errorf("Not enough parameters\n%s", commandUsage)
+					}
+					k8sVersion1 := c.Args().Get(0)
+					k8sVersion2 := c.Args().Get(1)
+					channelVersion := c.Args().Get(2)
+					channel := c.Args().Get(3)
+
+					validChannel, err := util.IsValidChannel(channel)
+					if !validChannel {
+						return fmt.Errorf("Not a valid channel: [%s], error [%v]", channel, err)
+					}
+
+					validChannelVersion, err := util.IsValidChannelVersion(channelVersion)
+					if !validChannelVersion {
+						return fmt.Errorf("Not a valid channel version: [%s], error [%v]", channel, err)
+					}
+
+					data, err := util.GetKDMDataFromURL(channel, channelVersion)
+					if err != nil {
+						return fmt.Errorf("Error while retrieving KDM data, error [%v]", err)
+					}
+
+					uniqueImagesK8sVersion1 := util.GetUniqueSystemImageList(data.K8sVersionRKESystemImages[k8sVersion1])
+					lenUniqueImagesK8sVersion1 := len(uniqueImagesK8sVersion1)
+					uniqueImagesK8sVersion2 := util.GetUniqueSystemImageList(data.K8sVersionRKESystemImages[k8sVersion2])
+					lenUniqueImagesK8sVersion2 := len(uniqueImagesK8sVersion2)
+
+					diffImages := util.Difference(uniqueImagesK8sVersion1, uniqueImagesK8sVersion2)
+
+					replyMessage := fmt.Sprintf("Images [%d] for Kubernetes version [%s] for channel [%s]:\n\n%s\n", lenUniqueImagesK8sVersion1, k8sVersion1, channel, strings.Join(uniqueImagesK8sVersion1, "\n"))
+					replyMessage = fmt.Sprintf("%s\nImages [%d] for Kubernetes version [%s] for channel [%s]:\n\n%s\n", replyMessage, lenUniqueImagesK8sVersion2, k8sVersion2, channel, strings.Join(uniqueImagesK8sVersion2, "\n"))
+					replyMessage = fmt.Sprintf("%s\nDifference:\n%s\n", replyMessage, strings.Join(diffImages, "\n"))
+					fmt.Printf(replyMessage)
+
+					return nil
+
+				},
+			},
+			{
+				Name:    "listk8saddons",
+				Aliases: []string{"lka"},
+				Usage:   "list k8s version addons",
+				Action: func(c *cli.Context) error {
+					commandUsage := fmt.Sprintf("Usage: %s <k8s_version> <channel_version> <channel>", c.Command.FullName())
+					if c.Args().Len() < 3 {
+						return fmt.Errorf("Not enough parameters\n%s", commandUsage)
+					}
+					k8sVersion := c.Args().Get(0)
+					channelVersion := c.Args().Get(1)
+					channel := c.Args().Get(2)
+
+					validChannel, err := util.IsValidChannel(channel)
+					if !validChannel {
+						return fmt.Errorf("Not a valid channel: [%s], error [%v]", channel, err)
+					}
+
+					validChannelVersion, err := util.IsValidChannelVersion(channelVersion)
+					if !validChannelVersion {
+						return fmt.Errorf("Not a valid channel version: [%s], error [%v]", channel, err)
+					}
+
+					data, err := util.GetKDMDataFromURL(channel, channelVersion)
+					if err != nil {
+						return fmt.Errorf("Error while retrieving KDM data, error [%v]", err)
+					}
+
+					k8sAddons := util.GetAddonNames(data.K8sVersionedTemplates)
+
+					replyMessage := fmt.Sprintf("Addons for Kubernetes version [%s] for channel [%s]:\n", k8sVersion, channel)
+
+					tableString := &strings.Builder{}
+					table := tablewriter.NewWriter(tableString)
+
+					table.SetHeader([]string{"Addon", "Template name"})
+					table.SetAutoWrapText(false)
+					table.SetAutoFormatHeaders(true)
+					table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+					table.SetAlignment(tablewriter.ALIGN_LEFT)
+					table.SetCenterSeparator("")
+					table.SetColumnSeparator("")
+					table.SetRowSeparator("")
+					table.SetHeaderLine(false)
+					table.SetBorder(false)
+					table.SetTablePadding("\t") // pad with tabs
+					table.SetNoWhiteSpace(true)
+
+					for _, addon := range k8sAddons {
+						templateName, _, err := util.GetTemplate(data.K8sVersionedTemplates, addon, k8sVersion)
+						if err != nil {
+							table.Append([]string{addon, fmt.Sprintf("%v", err)})
+							continue
+						}
+						table.Append([]string{addon, templateName})
+					}
+
+					table.Render()
+
+					replyMessage = fmt.Sprintf("%s\n%s\n", replyMessage, tableString.String())
+					fmt.Printf(replyMessage)
+
+					return nil
+
+				},
+			},
+			{
+				Name:    "diffk8saddons",
+				Aliases: []string{"dka"},
+				Usage:   "diff 2 k8s version addons",
+				Action: func(c *cli.Context) error {
+					commandUsage := fmt.Sprintf("Usage: %s <k8s_version1> <k8s_version2> <channel_version> <channel>", c.Command.FullName())
+
+					if c.Args().Len() < 4 {
+						return fmt.Errorf("Not enough parameters\n%s", commandUsage)
+					}
+					k8sVersion1 := c.Args().Get(0)
+					k8sVersion2 := c.Args().Get(1)
+					channelVersion := c.Args().Get(2)
+					channel := c.Args().Get(3)
+
+					validChannel, err := util.IsValidChannel(channel)
+					if !validChannel {
+						return fmt.Errorf("Not a valid channel: [%s], error [%v]", channel, err)
+					}
+
+					validChannelVersion, err := util.IsValidChannelVersion(channelVersion)
+					if !validChannelVersion {
+						return fmt.Errorf("Not a valid channel version: [%s], error [%v]", channel, err)
+					}
+
+					data, err := util.GetKDMDataFromURL(channel, channelVersion)
+					if err != nil {
+						return fmt.Errorf("Error while retrieving KDM data, error [%v]", err)
+					}
+					tableString := &strings.Builder{}
+					table := tablewriter.NewWriter(tableString)
+
+					table.SetHeader([]string{"Addon", k8sVersion1, k8sVersion2, "Diff?"})
+					table.SetAutoWrapText(false)
+					table.SetAutoFormatHeaders(true)
+					table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+					table.SetAlignment(tablewriter.ALIGN_LEFT)
+					table.SetCenterSeparator("")
+					table.SetColumnSeparator("")
+					table.SetRowSeparator("")
+					table.SetHeaderLine(false)
+					table.SetBorder(false)
+					table.SetTablePadding("\t") // pad with tabs
+					table.SetNoWhiteSpace(true)
+
+					k8sAddons := util.GetAddonNames(data.K8sVersionedTemplates)
+
+					for _, addon := range k8sAddons {
+						templateName1, _, err := util.GetTemplate(data.K8sVersionedTemplates, addon, k8sVersion1)
+						if err != nil {
+							templateName1 = fmt.Sprintf("Error: %v", err)
+						}
+						templateName2, _, err := util.GetTemplate(data.K8sVersionedTemplates, addon, k8sVersion2)
+						if err != nil {
+							templateName2 = fmt.Sprintf("Error: %v", err)
+						}
+						diff := "No"
+						if templateName1 != templateName2 {
+							diff = "Yes"
+						}
+						table.Append([]string{addon, templateName1, templateName2, diff})
+					}
+
+					table.Render()
+
+					replyMessage := fmt.Sprintf("%s\n", tableString.String())
+					fmt.Printf(replyMessage)
+
+					return nil
+
+				},
+			},
+		},
+	}
+	app.Version = Version
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ util/util.go
@@ -0,0 +1,253 @@
+package util
+
+import (
+	"fmt"
+	"io/ioutil"
+	"reflect"
+	"regexp"
+	"sort"
+	"strings"
+
+	"github.com/blang/semver/v4"
+	sv "github.com/coreos/go-semver/semver"
+	"github.com/hashicorp/go-retryablehttp"
+	kd "github.com/rancher/rancher/pkg/controllers/management/kontainerdrivermetadata"
+	ext "github.com/rancher/rancher/pkg/image/external"
+	rketypes "github.com/rancher/rke/types"
+	"github.com/rancher/rke/types/kdm"
+)
+
+func Difference(slice1 []string, slice2 []string) []string {
+	var diff []string
+
+	// Loop two times, first to find slice1 strings not in slice2,
+	// second loop to find slice2 strings not in slice1
+	for i := 0; i < 2; i++ {
+		for _, s1 := range slice1 {
+			found := false
+			for _, s2 := range slice2 {
+				if s1 == s2 {
+					found = true
+					break
+				}
+			}
+			// String not found. We add it to return slice
+			if !found {
+				diff = append(diff, s1)
+			}
+		}
+		// Swap the slices, only if it was the first loop
+		if i == 0 {
+			slice1, slice2 = slice2, slice1
+		}
+	}
+
+	return diff
+}
+
+func GetSemverFromString(version string) (semver.Version, error) {
+	strippedVersion := strings.TrimPrefix(version, "v")
+	semVersion, err := semver.Make(strippedVersion)
+	if err != nil {
+		return semver.Version{}, err
+	}
+	return semVersion, nil
+
+}
+
+func GetK8sVersionsForVersion(data kdm.Data, version string) ([]string, error) {
+	linuxInfo, _ := kd.GetK8sVersionInfo(
+		version,
+		data.K8sVersionRKESystemImages,
+		data.K8sVersionServiceOptions,
+		data.K8sVersionWindowsServiceOptions,
+		data.K8sVersionInfo,
+	)
+	var k8sVersions []string
+	for k := range linuxInfo.RKESystemImages {
+		k8sVersions = append(k8sVersions, k)
+	}
+	sort.Strings(k8sVersions)
+	return k8sVersions, nil
+}
+
+func GetRKE2K8sVersionsForVersion(data kdm.Data, version string) ([]string, error) {
+	var RKE2K8sVersions []string
+	rke2AllImages, err := ext.GetExternalImages(version, data.RKE2, ext.RKE2, &sv.Version{
+		Major: 1,
+		Minor: 21,
+		Patch: 0,
+	})
+	if err != nil {
+		return RKE2K8sVersions, err
+	}
+	for _, image := range rke2AllImages {
+		if strings.HasPrefix(image, "rancher/rke2-runtime") {
+			splitImage := strings.Split(image, ":")
+			if len(splitImage) == 2 {
+				RKE2K8sVersions = append(RKE2K8sVersions, splitImage[1])
+			}
+		}
+	}
+
+	sort.Strings(RKE2K8sVersions)
+	return RKE2K8sVersions, nil
+}
+
+func GetKDMDataFromURL(channel, channelVersion string) (kdm.Data, error) {
+	metadataURL := fmt.Sprintf("https://releases.rancher.com/kontainer-driver-metadata/%s-%s/data.json", channel, channelVersion)
+	retryClient := retryablehttp.NewClient()
+	retryClient.Logger = nil
+
+	req, err := retryablehttp.NewRequest("GET", metadataURL, nil)
+	if err != nil {
+		return kdm.Data{}, fmt.Errorf("error while calling NewRequest for [%s], error: %v", metadataURL, err)
+	}
+	resp, err := retryClient.Do(req)
+	if err != nil {
+		return kdm.Data{}, fmt.Errorf("error while executing HTTP request, error: %v", err)
+	}
+
+	//resp, err := retryablehttp.Get(metadataURL)
+	if err != nil || resp.StatusCode >= 400 {
+		return kdm.Data{}, fmt.Errorf("error during HTTP get to [%s], error: %v", metadataURL, err)
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return kdm.Data{}, fmt.Errorf("error during reading HTTP response body, error: %v", err)
+	}
+	data, err := kdm.FromData(b)
+	if err != nil {
+		return kdm.Data{}, fmt.Errorf("error during translating response body to KDM data, error: %v", err)
+	}
+	return data, nil
+}
+
+func GetKDMDataFromEmbedded(rancherVersion string) (kdm.Data, error) {
+	goModURL := fmt.Sprintf("https://raw.githubusercontent.com/rancher/rancher/%s/go.mod", rancherVersion)
+	retryClient := retryablehttp.NewClient()
+	retryClient.Logger = nil
+
+	req1, err := retryablehttp.NewRequest("GET", goModURL, nil)
+	if err != nil {
+		return kdm.Data{}, fmt.Errorf("error while calling NewRequest for [%s], error: %v", goModURL, err)
+	}
+	resp1, err := retryClient.Do(req1)
+	if err != nil || resp1.StatusCode >= 400 {
+		return kdm.Data{}, fmt.Errorf("error during HTTP get to [%s], error: %v", goModURL, err)
+	}
+
+	defer resp1.Body.Close()
+	goMod, err := ioutil.ReadAll(resp1.Body)
+	if err != nil {
+		return kdm.Data{}, fmt.Errorf("error during reading HTTP response body, error: %v", err)
+	}
+	re := regexp.MustCompile("(?m)github.com/rancher/rke\\s(.*)$")
+	match := re.FindStringSubmatch(string(goMod))
+	if len(match) < 1 {
+		return kdm.Data{}, fmt.Errorf("error during finding RKE version from go.mod")
+	}
+	rkeVersion := match[1]
+	splittedRKEVersion := strings.Split(rkeVersion, "-")
+	if len(splittedRKEVersion) >= 3 {
+		rkeVersion = splittedRKEVersion[2]
+	}
+	metadataURL := fmt.Sprintf("https://raw.githubusercontent.com/rancher/rke/%s/data/data.json", rkeVersion)
+	req2, err := retryablehttp.NewRequest("GET", metadataURL, nil)
+	if err != nil {
+		return kdm.Data{}, fmt.Errorf("error while calling NewRequest for [%s], error: %v", metadataURL, err)
+	}
+	resp2, err := retryClient.Do(req2)
+	if err != nil || resp2.StatusCode >= 400 {
+		return kdm.Data{}, fmt.Errorf("error during HTTP get to [%s], error: %v", goModURL, err)
+	}
+
+	defer resp2.Body.Close()
+	b, err := ioutil.ReadAll(resp2.Body)
+	if err != nil {
+		return kdm.Data{}, fmt.Errorf("error during reading 2nd HTTP response body, error: %v", err)
+	}
+	data, err := kdm.FromData(b)
+	if err != nil {
+		return kdm.Data{}, fmt.Errorf("error during translating response body to KDM data, error: %v", err)
+	}
+	return data, nil
+}
+
+func GetUniqueSystemImageList(rkeSystemImages rketypes.RKESystemImages) []string {
+	imagesReflect := reflect.ValueOf(rkeSystemImages)
+	var images []string
+	for i := 0; i < imagesReflect.NumField(); i++ {
+		if imagesReflect.Field(i).Interface().(string) == "" {
+			continue
+		}
+		images = append(images, imagesReflect.Field(i).Interface().(string))
+	}
+	return getUniqueSlice(images)
+}
+
+func getUniqueSlice(slice []string) []string {
+	encountered := map[string]bool{}
+	unqiue := []string{}
+
+	for i := range slice {
+		if encountered[slice[i]] {
+			continue
+		} else {
+			encountered[slice[i]] = true
+			unqiue = append(unqiue, slice[i])
+		}
+	}
+	return unqiue
+}
+
+func IsValidChannel(channel string) (bool, error) {
+	validChannels := []string{"dev", "release", "embedded"}
+	for _, validChannel := range validChannels {
+		if validChannel == channel {
+			return true, nil
+		}
+	}
+	return false, fmt.Errorf("not a valid channel [%s], valid options are [%s]", channel, strings.Join(validChannels, ","))
+}
+
+func IsValidChannelVersion(channelVersion string) (bool, error) {
+	match, err := regexp.MatchString("^v2\\.[4-9]", channelVersion)
+	if err != nil {
+		return false, fmt.Errorf("not a valid channel version [%s], error: %v", channelVersion, err)
+	}
+	if !match {
+		return false, fmt.Errorf("not a valid channel version [%s]", channelVersion)
+	}
+	return true, nil
+}
+
+func GetAddonNames(data map[string]map[string]string) []string {
+	var k8sAddons []string
+	for addon := range data {
+		if addon == "templateKeys" {
+			continue
+		}
+		k8sAddons = append(k8sAddons, addon)
+	}
+	return k8sAddons
+}
+
+func GetTemplate(data map[string]map[string]string, templateName, k8sVersion string) (string, string, error) {
+	versionData := data[templateName]
+	toMatch, err := semver.Make(k8sVersion[1:])
+	if err != nil {
+		return "", "", fmt.Errorf("k8sVersion not sem-ver %s %v", k8sVersion, err)
+	}
+	for k := range versionData {
+		testRange, err := semver.ParseRange(k)
+		if err != nil {
+			return "", "", fmt.Errorf("range for %s not sem-ver %v %v", templateName, testRange, err)
+		}
+		if testRange(toMatch) {
+			return versionData[k], data[kdm.TemplateKeys][versionData[k]], nil
+		}
+	}
+	return "", "", fmt.Errorf("no %s template found for k8sVersion %s", templateName, k8sVersion)
+}
